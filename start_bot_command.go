6b55c10f6c86566be97f8fb3a9175a350c3b5a35
Kalys Osmonov <kalys@osmonov.com>
2018-11-10 00:36:20 +0600

    Use mixpanel

--- start_bot_command.go
+++ start_bot_command.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/blevesearch/bleve"
 	"github.com/blevesearch/bleve/search"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/dukex/mixpanel"
 	"github.com/getsentry/raven-go"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"gopkg.in/urfave/cli.v2" // imports as package "cli"
+	"strconv"
 	_ "strconv"
 	"strings"
 	"time"
@@ -32,10 +33,16 @@ var StartBotCommand = cli.Command{
 			Usage: "DSN for sentry",
 			Value: "some-dsn",
 		},
+		&cli.StringFlag{
+			Name:  "mixpanel-token",
+			Usage: "Mixpanel token",
+			Value: "some-token",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		spew.Dump()
 		raven.SetDSN(c.String("raven-dsn"))
+		mixpanelClient := mixpanel.New(c.String("mixpanel-token"), "")
 
 		index, err := bleve.Open(c.String("index-path"))
 		if err != nil {
@@ -51,23 +58,22 @@ var StartBotCommand = cli.Command{
 			panic(err)
 		}
 
-		searchFunction := func(term string) *bleve.SearchResult {
+		searchFunction := func(term string) (*bleve.SearchResult, error) {
 			query := bleve.NewQueryStringQuery(term)
 
 			searchRequest := bleve.NewSearchRequest(query)
 			searchRequest.Fields = []string{"Keyword", "Value"}
-			searchResult, _ := index.Search(searchRequest)
-			return searchResult
+			return index.Search(searchRequest)
 		}
 
-		firstHit := func(sr *bleve.SearchResult) (string, error) {
+		firstHit := func(sr *bleve.SearchResult) string {
 			if len(sr.Hits) == 0 {
-				return "", errors.New("No Hits")
+				return ""
 			}
 			hitValue := strings.Replace(sr.Hits[0].Fields["Value"].(string), "&nbsp;", "", -1)
 			hitKeyword := strings.Join([]string{"<b>", sr.Hits[0].Fields["Keyword"].(string), "</b>"}, "")
 
-			return strings.Join([]string{hitKeyword, hitValue}, "\n"), nil
+			return strings.Join([]string{hitKeyword, hitValue}, "\n")
 		}
 
 		buttons := func(hits search.DocumentMatchCollection, query string) [][]tb.InlineButton {
@@ -110,14 +116,42 @@ var StartBotCommand = cli.Command{
 				query := bleve.NewDocIDQuery([]string{wordID})
 				searchRequest := bleve.NewSearchRequest(query)
 				searchRequest.Fields = []string{"Keyword", "Value"}
-				docIDSearchResult, _ := index.Search(searchRequest)
+				docIDSearchResult, err := index.Search(searchRequest)
+				if err != nil {
+					raven.CaptureError(err, map[string]string{"term": term})
+					b.Respond(c, &tb.CallbackResponse{
+						CallbackID: c.ID,
+						Text:       "Произошла ошибка. Попробуйте выбрать другой перевод",
+					})
+					return
+				}
 
-				searchResult := searchFunction(term)
+				searchResult, err := searchFunction(term)
+				if err != nil {
+					raven.CaptureError(err, map[string]string{"term": term})
+					b.Respond(c, &tb.CallbackResponse{
+						CallbackID: c.ID,
+						Text:       "Произошла ошибка. Попробуйте ввести другое ключевое слово",
+					})
+					return
+				}
+
 				buttons := buttons(searchResult.Hits, term)
+				messageText := firstHit(docIDSearchResult)
 
-				messageText, err := firstHit(docIDSearchResult)
-
-				if err == nil {
+				if messageText == "" {
+					b.Respond(c, &tb.CallbackResponse{
+						CallbackID: c.ID,
+						Text:       "Произошла ошибка. Попробуйте выбрать другой перевод",
+					})
+					return
+				} else {
+					selectedKeyword := docIDSearchResult.Hits[0].Fields["Keyword"].(string)
+					mixpanelClient.Track(strconv.Itoa(c.Sender.ID), "Selected", &mixpanel.Event{
+						Properties: map[string]interface{}{
+							"keyword": selectedKeyword,
+						},
+					})
 					b.Edit(c.Message,
 						messageText,
 						&tb.SendOptions{
@@ -137,14 +171,32 @@ var StartBotCommand = cli.Command{
 
 		b.Handle(tb.OnText, func(m *tb.Message) {
 			raven.CapturePanic(func() {
-				term := fmt.Sprintf("Keyword:%s^5 Value:%s", m.Text, m.Text)
+				term := m.Text
+				queryString := fmt.Sprintf("Keyword:%s^5 Value:%s", term, term)
 
-				searchResult := searchFunction(term)
+				searchResult, err := searchFunction(queryString)
+				if err != nil {
+					raven.CaptureError(err, map[string]string{"term": term})
+					return
+				}
+
 				buttons := buttons(searchResult.Hits, term)
 
-				messageText, err := firstHit(searchResult)
+				messageText := firstHit(searchResult)
 
-				if err == nil {
+				if messageText == "" {
+					mixpanelClient.Track(strconv.Itoa(m.Sender.ID), "Not found", &mixpanel.Event{
+						Properties: map[string]interface{}{
+							"term": term,
+						},
+					})
+					b.Send(m.Sender, "Перевод не найден")
+				} else {
+					mixpanelClient.Track(strconv.Itoa(m.Sender.ID), "Translate", &mixpanel.Event{
+						Properties: map[string]interface{}{
+							"term": term,
+						},
+					})
 					b.Send(m.Sender,
 						messageText,
 						&tb.SendOptions{
